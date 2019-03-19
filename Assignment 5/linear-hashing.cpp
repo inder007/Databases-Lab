65c23b6d171e8cfab917cf688d22dbfa2bd4a3a0
inder007 <inder007>
2019-03-19 23:17:00 +0530

    code complete with bugs

--- Assignment 5/linear-hashing.cpp
+++ Assignment 5/linear-hashing.cpp
@@ -20,41 +20,93 @@ struct bucket{
     int ct;
 };
 
+struct head{
+    bucket* h;
+};
+
 bucket* createBucket(){
-    bucket* temp;
+    bucket* temp = new bucket;
     temp->ct=0;
     return temp;
 }
 
-bucket* insertBucket(bucket* a, int i){
-    if(a==NULL){
-        a=createBucket();
+int getBucketNo(int n,int l,int next){
+    if(n%l > next){
+        return n%l;
     }
-    a->arr.pb(i);
-    a->ct++;
-    return a;
+    else{
+        return n%(2*l);
+    }
+    // return 0;
 }
 
-int getBucketNo(int n){
-    return 0;
+void insertBucket(vector<head> &a, int index, int n,int &next, int &level){
+    if(a[index].h == NULL){
+        a[index].h = createBucket();
+    }
+    a[index].h->ct++;
+    a[index].h->arr.push_back(n);
+    if(a[index].h->ct<=MAXSIZE){
+        return;
+    }
+    else{
+        head temp;
+        temp.h=NULL;
+        a.push_back(temp);
+        if(index==next){
+            next=(next+1)%(level);
+            bucket* t = a[index].h;
+            a[index].h=NULL; 
+            for(int i=0;i<t->ct;i++){
+                int x = getBucketNo(a[index].h->arr[i],level,next);
+                if(a[x].h == NULL){
+                    a[x].h = createBucket();
+                }
+                a[x].h->ct++;
+                a[x].h->arr.push_back(n);
+            }
+            if(next==0){
+                level=2*level;
+            }
+        }
+    }
+    // return a;
 }
+
 
 int main(){
-    vector<bucket*> plist(1);
-    bucket* crrPointer=plist[0];
+    vector<head> heads(2);
+    // bucket* next=plist[0];
     // bucket* a=createBucket();
     // plist.pb(a);
-    int bitcount=0;
-    int size=0;
+    int level=2;
+    int next=0,n,index;
     while(1){
         char in;
         cin>>in;
         switch(in){
-            case 'i':   
-                int n;
+            case 'i': 
                 cin>>n;
-
-
+                index = getBucketNo(n,level,next);
+                insertBucket(heads,index,n,next,level);
+                break;
+            case 'p' :
+                for(int i=0;i<heads.size();i++){
+                    if(heads[i].h != NULL){
+                        for(int j=0;j<heads[i].h->arr.size();j++){
+                            cout<<heads[i].h->arr[i]<<" ";
+                        }
+                        cout<<endl;
+                    }
+                    else{
+                        cout<<"NULL"<<endl;
+                    }
+                }
+                break;
+            case 'q' :
+                return 0;
+            default :
+                break;
         }
     }
     return 0;
